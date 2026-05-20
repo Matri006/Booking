dd8880bb307461cb935340a447a08487f6fa283e
Masha <mari.dorofeeva.06@bk.ru>
2026-05-21 01:17:21 +0300

    feat: added repository layer

---
+++ src/main/java/com/mary/booking/repository/BookingRepository.java
@@ -0,0 +1,17 @@
+package com.mary.booking.repository;
+
+import com.mary.booking.entity.Booking;
+import com.mary.booking.entity.Room;
+import com.mary.booking.enums.BookingStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    boolean existsByRoomAndStatusAndStartTimeLessThanAndEndTimeGreaterThan(
+            Room room,
+            BookingStatus status,
+            LocalDateTime endTime,
+            LocalDateTime startTime
+    );
+}
